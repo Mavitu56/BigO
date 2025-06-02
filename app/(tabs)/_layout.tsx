9ade7cf5aaeffa30becd50e38f42411cf3a26c33
SofiaFO <ferreiradeoliveirasofia@gmail.com>
2025-06-02 12:12:12 -0300

    feat: adicionando o front

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,21 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+
+import { HapticTab } from '@/components/HapticTab';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+import TabBarBackground from '@/components/ui/TabBarBackground';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}>
+    </Stack>
+  );
+}
---
+++ app/(tabs)/perfil.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function PerfilScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Perfil Screen</Text>
+    </View>
+  );
+}
